6f79db8fef9cb22e5460f6a436a36f3da2e7d68a
Matt Artingstall <matt@imagndesign.com>
2018-09-25 09:34:10 -0700

    Initial set up

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="amobee-api-client",
+    version="0.1dev",
+    author="Matt Artingstall",
+    author_email="matt@imagndesign.com",
+    description="Simple wrapper for Amobee API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/martingstall/amobee-api-client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7.3",
+        "License :: GNU GENERAL PUBLIC LICENSE",
+        "Operating System :: OS Independent",
+    ],
+)
