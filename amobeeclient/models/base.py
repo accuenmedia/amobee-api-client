9a6481326d80d30b868202e2fb6f58f41b0b0015
Matt Artingstall <matt@imagndesign.com>
2018-12-05 10:59:11 -0800

    Create()

--- amobeeclient/models/base.py
+++ amobeeclient/models/base.py
@@ -10,6 +10,13 @@ class Base:
     def __init__(self, connection=None):
         self.connection = connection
 
+    def api_headers(self):
+        headers = {}
+        headers['Content-Type'] = 'application/json'
+        headers['Authorization'] = 'Bearer {0}'.format(self.connection.access_token)
+
+        return headers
+
     def get_by_id(self, id):
         url = "{0}/{1}/{2}".format(self.url_metadata, self.object, id)
         response = requests.get(
@@ -17,6 +24,7 @@ class Base:
             headers=self.api_headers(),
             verify=False
         )
+
         return self.get_response_object(response)
 
     def list_objects(self, url_params=''):
@@ -26,14 +34,19 @@ class Base:
             headers=self.api_headers(),
             verify=False
         )
+
         return self.get_response_list(response)
 
-    def api_headers(self):
-        headers = {}
-        headers['Content-Type'] = 'application/json'
-        headers['Authorization'] = 'Bearer {0}'.format(self.connection.access_token)
+    def create(self, payload):
+        url = "{0}/{1}/".format(self.url_metadata, self.object)
+        response = requests.post(
+            url,
+            headers=self.api_headers(),
+            data=payload,
+            verify=False
+        )
 
-        return headers
+        return self.get_response_object(response)
 
     def get_response_list(self, response):
         data = json.loads(response.text)
