8ee7937f6ec73329da569fc7527844c51b758c66
Matt Artingstall <matt@imagndesign.com>
2019-04-18 07:34:50 -0700

    Line item creative ids must go through ads

--- amobeeclient/models/creative.py
+++ amobeeclient/models/creative.py
@@ -9,13 +9,19 @@ class Creative(Base):
     object = "creatives"
 
     def find_by_line_item(self, id):
-        url = "{0}/{1}?lineItemId={2}".format(self.url_metadata, self.object, id)
+        """
+        Their "creatives" endpoint has no direct method to get assigned line items.
+        Must go through the "ads" endpoint: https://services.amobee.com/campaign/v3/doc/#/Ads/getAdsUsingGET
+
+        :param id: string
+        :return: JSON object
+        """
+        url = "{0}/{1}?lineItemId={2}".format(self.url_metadata, 'ads', id)
         response = requests.get(
             url,
             headers=self.api_headers(),
             verify=False
         )
-
         return self.get_response_list(response)
 
     def find_by_advertiser(self, id):
